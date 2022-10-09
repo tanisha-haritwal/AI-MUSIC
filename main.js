76c6de99290d012c8ca3baaa02433da10af52350
tanisha-haritwal <86518130+tanisha-haritwal@users.noreply.github.com>
2022-10-09 15:51:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
 song1=loadSound("music.mp3");
 song2=loadSound("music2.mp3");
@@ -9,7 +13,24 @@ canvas= createCanvas(600, 500);
 canvas.center();
 video= createCapture(VIDEO);
 video.hide();
+poseNet= ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
 function draw(){
 image(video, 0, 0, 600, 500);
+}
+function modelLoaded(){
+console.log('PoseNet Is Initialized');
+}
+function gotPoses(results){
+if(results.length > 0){
+console.log(results);
+leftWristX = results[0].pose.leftWrist.x;
+leftWristY = results[0].pose.leftWrist.y;
+console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+rightWristX= results[0].pose.rightWrist.x;
+rightWristY= results[0].pose.rightWrist.y;
+console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+}
 }
